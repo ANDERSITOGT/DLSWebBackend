63e88c5b36bfb4377d4db6b12f04fbb8c8c24b92
ANDERSITOGT <lopezanderson4182@gmail.com>
2025-11-20 11:05:50 -0600

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,19 +1,14 @@
-// src/test.ts
-import { prisma } from "./prisma";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 async function main() {
-  // Crear una unidad de prueba
-  await prisma.unidad.create({
-    data: {
-      nombre: "Kilogramos",
-      abreviatura: "kg"
-    }
-  });
-
-  // Obtener todas las unidades
   const unidades = await prisma.unidad.findMany();
   console.log(unidades);
 }
 
-main();
-
+main()
+  .then(() => prisma.$disconnect())
+  .catch((e) => {
+    console.error(e);
+    prisma.$disconnect();
+  });
